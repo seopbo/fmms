92262a101e3d921f1ee7f287af926878030122c2
aisolab <bsk0130@gmail.com>
2020-05-16 09:31:03 +0900

    app 구동관련 쉘 스크립트 추가

--- run_app.sh
+++ run_app.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env zsh
 
-# 가상환경을 activate
-source ~/.zshrc
-pyenv activate fmms
+args=$1
 
-# flask 웹 어플리케이션은 development 모드로 run
-export FLASK_ENV=development # flask가 실행되는 개발 스테이지, development로 정해놓으면 debug mode 실행
-export FLASK_app=app.py
-flask run
+case $1 in
+  run)
+    echo "run app"
+    # flask 웹 어플리케이션은 development 모드로 run
+    export FLASK_ENV=development # flask가 실행되는 개발 스테이지, development로 정해놓으면 debug mode 실행
+    export FLASK_app=app.py
+    screen -dmS flask-server zsh -c "source ~/.zshrc && pyenv activate fmms && flask run"
+    ;;
+  kill)
+    echo "kill app"
+    screen -S flask-server -X quit
+    ;;
+esac
